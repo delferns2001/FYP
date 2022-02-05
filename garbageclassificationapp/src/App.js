8dba18da8e25a2f7ed373e49ec0c3fd1662c4520
delferns2001 <66013004+delferns2001@users.noreply.github.com>
2022-02-05 00:47:06 +0000

    Added the initial outline and added some trained models

---
+++ garbageclassificationapp/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import TopNavBar from "./Components/Navbar";
+import Homepage from "./Components/HomePage";
+
+function App() {
+    return (
+        <>
+            <TopNavBar></TopNavBar>
+            <Homepage></Homepage>
+        </>
+    );
+}
+
+export default App;
